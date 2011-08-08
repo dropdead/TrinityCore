77bafe46cb012ddd9ae74c0c87f6c6424d875e52
Retriman <nicolas_katre@hotmail.com>
2011-08-07 21:33:02 -0400

    Core/pets:Fix de stats de pets sumoneados por npcs

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -1113,6 +1113,10 @@ void Creature::SelectLevel(const CreatureTemplate *cinfo)
 {
     uint32 rank = isPet()? 0 : cinfo->rank;
 
+    // Fix de stats de pets sumoneados por npcs
+    if (isPet() && GetOwner() && GetOwner()->GetTypeId() == TYPEID_UNIT)
+        return;
+
     // level
     uint8 minlevel = std::min(cinfo->maxlevel, cinfo->minlevel);
     uint8 maxlevel = std::max(cinfo->maxlevel, cinfo->minlevel);
