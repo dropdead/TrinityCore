3b0f3dbd625e7713b8c8baba4c41d2b828acd8ce
Retriman <nicolas_katre@hotmail.com>
2011-08-09 19:13:35 -0400

    Db: Fix Chance Plantas, Orden sqls gral. añadidos al full correspondiente.

--- sql/Madboxpc_update/Full/Gral_Full.sql
+++ sql/Madboxpc_update/Full/Gral_Full.sql
@@ -1,3 +1,6 @@
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
 -- Vendors_season08.sql
 /*
 -- dalaran
@@ -142,6 +145,9 @@ insert into `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equip
 
 
 
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
 -- Npcs Sin Exp.
 UPDATE `creature_template` SET `flags_extra` = 64 WHERE `entry` IN
 (691,
@@ -445,3 +451,20 @@ UPDATE `creature_template` SET `flags_extra` = 64 WHERE `entry` IN
 31010,
 30962,
 30662);
+
+
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
+-- Solucionando Problema Mining Northerend Reportado en http://code.google.com/p/madboxpcwow/issues/detail?id=19&q=owner:Retriman#makechanges
+-- SELECT * FROM `pool_template` WHERE `entry` IN (5181,899,5000,5121,898,897,896);
+-- Fix Test Pool Entry
+UPDATE `pool_template` t SET `max_limit`=(SELECT COUNT(pool_entry) FROM pool_gameobject WHERE pool_entry=t.entry)/2 WHERE max_limit > 5; 
+UPDATE `pool_template` SET `max_limit` =21 WHERE `entry` =5181;-- Grizzly Hills Mineral MASTER
+UPDATE `pool_template` SET `max_limit` =63 WHERE `entry` =899;-- MASTER Minerals Sholazar Basin zone 3711
+UPDATE `pool_template` SET `max_limit` =32 WHERE `entry` =5000;-- Borean Tundra Mineral MASTER
+UPDATE `pool_template` SET `max_limit` =30 WHERE `entry` =5121;-- Howling Fjord Mineral MASTER
+UPDATE `pool_template` SET `max_limit` =48 WHERE `entry` =898;-- MASTER Minerals Storm Peaks zone 67
+UPDATE `pool_template` SET `max_limit` =69 WHERE `entry` =897;-- MASTER Minerals Icecrown zone 210
+UPDATE `pool_template` SET `max_limit` =32 WHERE `entry` =896;-- MASTER Minerals Wintergrasp zone 4197
+
