46c16a1655733baff6b2b41417b21a5d8956572f
Barneysk8 <adrian_mardones@hotmail.com>
2011-09-08 01:45:17 -0300

    Scripts/Quest: [Quest] Challenge to the Black Flight (Q 11162) Unit Flags

--- src/server/scripts/Kalimdor/dustwallow_marsh.cpp
+++ src/server/scripts/Kalimdor/dustwallow_marsh.cpp
@@ -888,8 +888,8 @@ public:
     {
        npc_smolderwingAI(Creature* c) : ScriptedAI(c) 
        { 
-           me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
-           me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+           me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PASSIVE);
+           me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_OOC_NOT_ATTACKABLE);
        }
 
         void EnterCombat(Unit* pUnit)
