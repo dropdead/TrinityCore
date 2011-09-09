74fc0998879dcf47b30b8c62b53c2130474aeff6
Barneysk8 <adrian_mardones@hotmail.com>
2011-09-09 00:31:19 -0300

    [Quest] No Mercy for the Captured

---
+++ sql/Madboxpc_update/Quest/06_[Quest][12245]No_Mercy_for_the_Captured.sql
@@ -0,0 +1,3 @@
+-- Quest] No Mercy for the Captured
+--  http://www.wowhead.com/quest=12245#
+UPDATE `creature_template`SET `ScriptName` = 'npc_no_mercy_for_the_captured' WHERE `entry` IN (27376,27378,27379,27381);
