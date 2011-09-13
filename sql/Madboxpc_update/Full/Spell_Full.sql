58d4a92b1cbca903af6b9746d4aebd8245118081
Retriman <nicolas_katre@hotmail.com>
2011-09-13 00:04:10 -0400

    Core/Spells: Fix [Skill] Blessing of Sanctuary. Close #125

--- sql/Madboxpc_update/Full/Spell_Full.sql
+++ sql/Madboxpc_update/Full/Spell_Full.sql
@@ -43,3 +43,11 @@ INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `Spell
 -- Fix Spell Explosive Trap (Hunter) daño periódico nulo
 UPDATE `spell_bonus_data` SET `ap_dot_bonus` = '0.1' WHERE `entry` = 13812;
 
+-- 08[Paladin]Blessing_of_Sanctuary.sql
+DELETE FROM `spell_dbc` WHERE `id` = 20912;
+INSERT INTO `spell_dbc` (`Id`,`CastingTimeIndex`,`DurationIndex`,`RangeIndex`,`Effect1`,`EffectBasePoints1`,`EffectImplicitTargetA1`,`EffectApplyAuraName1`,`EffectMiscValue1`,`SpellFamilyName`,`Comment`) VALUES
+(20912,1,21,1,6,-3,1,87,127,10,'Blessing of Sanctuary Helper (SERVERSIDE)');
+-- Blessing of Sanctuary vs Vigilance
+UPDATE `spell_group` SET `spell_id` = 68066 WHERE `id` = 1091 and `spell_id` = 47930;
+UPDATE `spell_group` SET `spell_id` = 20912 WHERE `id` = 1092 and `spell_id` = 20911;
+
