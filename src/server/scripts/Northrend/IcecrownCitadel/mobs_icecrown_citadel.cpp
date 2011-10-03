77b3038810ff27381ad9c5e94a56a96dacb7fd3b
Retriman <nicolas_katre@hotmail.com>
2011-10-02 21:53:16 -0400

    Scripts/Icecrown: Solucioando errores logicos, y añadidoendo partes faltantes en el commit anterior.

--- src/server/scripts/Northrend/IcecrownCitadel/mobs_icecrown_citadel.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/mobs_icecrown_citadel.cpp
@@ -17,25 +17,56 @@ enum spells
     SPELL_POWER_WORD_SHIELD_10  = 71780, // http://old.wowhead.com/spell=71780 - Mode 10 player
     SPELL_POWER_WORD_SHIELD_25  = 71781, // http://old.wowhead.com/spell=71781 - Mode 25 player
     SPELL_POWER_WORD_SHIELD_ALL = 71548, // http://old.wowhead.com/spell=71548 - Mode todos
-    SPELL_SMITE_10  			= 71546, // http://old.wowhead.com/spell=71546
-    SPELL_SMITE_10_HEROIC  		= 71778, // http://old.wowhead.com/spell=71778
-    SPELL_SMITE_25  			= 71779, // http://old.wowhead.com/spell=71779
-    SPELL_SMITE_25_HEROIC 		= 71547, // http://old.wowhead.com/spell=71547
-    SPELL_UNDEAD_ALL 			= 70089, // http://old.wowhead.com/spell=70089
+    SPELL_SMITE_10              = 71546, // http://old.wowhead.com/spell=71546
+    SPELL_SMITE_10_HEROIC       = 71778, // http://old.wowhead.com/spell=71778
+    SPELL_SMITE_25              = 71779, // http://old.wowhead.com/spell=71779
+    SPELL_SMITE_25_HEROIC       = 71547, // http://old.wowhead.com/spell=71547
+    SPELL_UNDEAD_ALL            = 70089, // http://old.wowhead.com/spell=70089
     // Npc Deathbound Ward - http://old.wowhead.com/npc=37007
     SPELL_DISRUPTING_SHOUT_ALL  = 71022, // http://old.wowhead.com/spell=71022
-    SPELL_SABER_LASH_ALL		= 71021, // http://old.wowhead.com/spell=71021
-    SPELL_SOUL_FEAST_ALL 		= 71203, // http://old.wowhead.com/spell=71203
+    SPELL_SABER_LASH_ALL        = 71021, // http://old.wowhead.com/spell=71021
+    SPELL_SOUL_FEAST_ALL        = 71203, // http://old.wowhead.com/spell=71203
     // Npc Captain Brandon - http://old.wowhead.com/npc=37123
     SPELL_CRUSADER_STRIKE       = 71549, // http://old.wowhead.com/spell=71549
     SPELL_DIVINE_SHIELD         = 71550, // http://old.wowhead.com/spell=71550
     SPELL_HAMMER_OF_BETRAYAL    = 71784, // http://old.wowhead.com/spell=71784
     SPELL_JUDGEMENT_OF_COMMAND  = 71551, // http://old.wowhead.com/spell=71551
-    SPELL_UNDEATH               = 70089, // http://old.wowhead.com/spell=70089
 };
 
-// Aca definimos spell para 4 modos es 10normal/10heroic/25normal/25heroic
-#define SPELL_SMITE_MODES  RAID_MODE <uint32> (SPELL_SMITE_10,SPELL_SMITE_10_HEROIC,SPELL_SMITE_25,SPELL_SMITE_25_HEROIC);
+struct mobs_icecrown_citadelAI : public ScriptedAI
+{
+    mobs_icecrown_citadelAI(Creature *c) : ScriptedAI(c)
+    {
+        if(c->GetMap()->IsDungeon())
+            instance = c->GetInstanceScript();
+
+        c->setActive(true);
+    }
+
+    void EnterCombat(Unit *who)
+    {
+        DoAttackerAreaInCombat(who, 100);
+        Unit *target = SelectTarget(SELECT_TARGET_RANDOM, 0);
+    }
+
+    bool DoCastTry(Unit *victim, uint32 spellId, bool triggered = false)
+    {
+        if(me->IsNonMeleeSpellCasted(false) && !triggered) return false;
+
+        DoCast(victim,spellId,triggered);
+        return true;
+    }
+
+    bool DoCastTryAOE(uint32 spellId, bool triggered = false)
+    {
+        if(me->IsNonMeleeSpellCasted(false) && !triggered) return false;
+
+        DoCastAOE(spellId,triggered);
+        return true;
+    }
+
+    InstanceScript* instance;
+};
 
 class mobs_icecrown_citadel : public CreatureScript
 {
@@ -75,18 +106,17 @@ public:
             Dominate_Mind       = 5000;
             Flash_heal          = urand(15000,25000);
             Power_word_shield   = urand(30000,20000);
-            Smite               = urand (5000,10000);
+            Smite               = 10000;
         }
 
         void EnterCombat(Unit *who)
         {
             mobs_icecrown_citadelAI::EnterCombat(who);
-            DoCast(me,POWER_WORD_SHIELD_ALL);
+            DoCast(me,SPELL_POWER_WORD_SHIELD_ALL);
         }
 
         void JustDied(Unit* killer)
         {
-            mobs_icecrown_citadelAI::JustDied(who);
             DoCast(me,SPELL_UNDEAD_ALL);
         }
 
@@ -97,7 +127,7 @@ public:
 
             if (Dominate_Mind <= diff)
             {
-                if (TryDoCast(SPELL_DOMINATE_MIND))
+                if (DoCastTry(me,SPELL_DOMINATE_MIND))
                     Dominate_Mind = 15000;
             }else Dominate_Mind -= diff;
 
@@ -105,24 +135,24 @@ public:
             {
                 // marcamos 1 target rando a 40 yardas
                 if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 1,40,false))
-                    if (TryDoCast(target,(SPELL_SMITE_MODES))
-                        Smite = 10000; // tiempo generico para todas dificultades.
+                    if (DoCastTry(target,RAID_MODE(SPELL_SMITE_10,SPELL_SMITE_10_HEROIC,SPELL_SMITE_25,SPELL_SMITE_25_HEROIC)))
+                        Smite = 15000; // tiempo generico para todas dificultades.
             }else Smite -= diff;
 
             if (Flash_heal <= diff)
             {
                 // Aca como la spell Flash heal se utiliza en dos dificultades usamos RAID_MODE que tomara la dificultad de la insta.
-                if (TryDoCast(me,RAID_MODE(SPELL_FLASH_HEAL_MODE_10,SPELL_FLASH_HEAL_MODE_25)))
+                if (DoCastTry(me,RAID_MODE(SPELL_FLASH_HEAL_MODE_10,SPELL_FLASH_HEAL_MODE_25)))
                     Flash_heal = urand(2000,7000);
             }else Flash_heal -= diff;
 
             if (Power_word_shield <= diff)
             {
-                if (TryDoCast(me,RAID_MODE(SPELL_POWER_WORD_SHIELD_10,SPELL_POWER_WORD_SHIELD_25)))
+                if (DoCastTry(me,RAID_MODE(SPELL_POWER_WORD_SHIELD_10,SPELL_POWER_WORD_SHIELD_25)))
                     Power_word_shield = urand(5000,15000);
             }else Power_word_shield -= diff;
 
-            mobs_icecrown_citadelAIAI::UpdateAI(diff);
+            mobs_icecrown_citadelAI::UpdateAI(diff);
         }
     };
 
@@ -136,14 +166,13 @@ public:
         void Reset()
         {
             Disrupting_Shout    = 10000;
-            Saber_Lash          =  7000;
+            Saber_Lash          = 7000;
         }
 
         /*void JustDied(Unit* killer)
         {
-            mobs_icecrown_citadelAI::JustDied(who);
             // aca falta agregar 	50000 yards (Anywhere)
-		}*/
+        }*/
 
         void UpdateAI(const uint32 diff)
         {
@@ -152,15 +181,15 @@ public:
 
             if (Saber_Lash <= diff)
             {
-                if (TryDoCast(SPELL_SABER_LASH_ALL))
+                if (DoCastTry(me,SPELL_SABER_LASH_ALL))
                     Saber_Lash = 7000;
             }else Saber_Lash -= diff;
 
-            if (SPELL_DISRUPTING_SHOUT_ALL <= diff)
+            if (Disrupting_Shout <= diff)
             {
                 // Random al interrumpir spells con Disrupting_Shout
                 if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 1,40,false))
-                    if (TryDoCast(target,(SPELL_DISRUPTING_SHOUT_ALL))
+                    if (DoCastTry(target,(SPELL_DISRUPTING_SHOUT_ALL)))
                         Disrupting_Shout = 10000;
             }else Disrupting_Shout -= diff;
 
@@ -178,15 +207,14 @@ public:
 
         void Reset()
         {
-            Crusader_Strike = 5000;
-            Hammmer_Betrayal = 25000;
-            Judgement_Of_Command = 30000;
+            Crusader_Strike         = 5000;
+            Hammmer_Betrayal        = 25000;
+            Judgement_Of_Command    = 30000;
         }
 
         void JustDied(Unit* killer)
         {
-            mobs_icecrown_citadelAI::JustDied(who);
-            DoCast(me,SPELL_UNDEATH);
+            DoCast(me,SPELL_UNDEAD_ALL);
         }
 
         void UpdateAI(const uint32 diff)
@@ -202,7 +230,7 @@ public:
 
             if (Crusader_Strike <= diff)
             {
-                if (TryDoCast(me->getVictim(),SPELL_CRUSADER_STRIKE))
+                if (DoCastTry(me->getVictim(),SPELL_CRUSADER_STRIKE))
                     Crusader_Strike = 15000;
             }else Crusader_Strike -= diff;
 
@@ -210,13 +238,13 @@ public:
             {
                 // Marcamos 1 target random a 40 yardas
                 if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 1,40,false))
-                    if (TryDoCast(target,(SPELL_HAMMER_OF_BETRAYAL))
+                    if (DoCastTry(target,(SPELL_HAMMER_OF_BETRAYAL)))
                         Hammmer_Betrayal = 10000; 
             }else Hammmer_Betrayal -= diff;
 
             if (Judgement_Of_Command <= diff)
             {
-                if (TryDoCast(me->getVictim(),SPELL_JUDGEMENT_OF_COMMAND))
+                if (DoCastTry(me->getVictim(),SPELL_JUDGEMENT_OF_COMMAND))
                     Judgement_Of_Command = 15000;
             }else Judgement_Of_Command -= diff;   
 
