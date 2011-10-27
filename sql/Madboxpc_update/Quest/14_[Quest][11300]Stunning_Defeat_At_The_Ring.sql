79dc53c33d497a42dab9db43c5908c101bd3e016
Retriman <nicolas_katre@hotmail.com>
2011-10-27 17:01:01 -0300

    Quest: Fix Quest Quest: Stunning Defeat at The Ring http://www.wowhead.com/quest=11300. Update #286

---
+++ sql/Madboxpc_update/Quest/14_[Quest][11300]Stunning_Defeat_At_The_Ring.sql
@@ -0,0 +1,6 @@
+-- Fix Quest Quest: Stunning Defeat at The Ring
+-- http://www.wowhead.com/quest=11300
+DELETE FROM `creature` WHERE `id` =23931;
+INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`)
+VALUES
+('23931','571','1','1','0','519','798.106','-4706.17','-96.144','4.93175','300','0','0','15968','0','0');
