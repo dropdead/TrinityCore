3cb8245614a5c283301757f7e0a8aa779778ccb3
Retriman <nicolas_katre@hotmail.com>
2011-10-30 12:51:59 -0300

    ICC: Fix Npc http://www.wowhead.com/npc=37697 añadido correspondiente mechanic_immune_mask. Npc usado en Putricide

--- sql/Madboxpc_update/Dungeon/11_[Dungeon]Icecrown_Citadel.sql
+++ sql/Madboxpc_update/Dungeon/11_[Dungeon]Icecrown_Citadel.sql
@@ -1,7 +1,15 @@
 -- ICC
 
 -- BOSS FESTERGUT
+-- Fix spell http://www.wowhead.com/spell=71160
 DELETE FROM `spell_script_names` WHERE `spell_id` IN (71160,71161);
 INSERT INTO `spell_script_names` VALUES 
 (71160,'spell_stinky_plague_stench'),
 (71161,'spell_stinky_plague_stench');
+
+
+-- BOSS PUTRICIDE
+-- http://www.wowhead.com/npc=37697
+-- 
+-- Añadido correcto inmune mask a npcs (SELECT  `entry` ,  `difficulty_entry_1` ,  `difficulty_entry_2` ,  `difficulty_entry_3` FROM  `creature_template` WHERE  `entry`IN ( 37697 )); 
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|1|2|8|16|32|64|128|256|512|1024|2048|4096|8192|65536|131072|524288|4194304|8388608|33554432|67108864|536870912 WHERE `entry` IN (37697,38604,38758,38759);
