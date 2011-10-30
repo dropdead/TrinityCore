0679cfe9924c64e3bb795ea2bde7541d0ac4041c
Retriman <nicolas_katre@hotmail.com>
2011-10-30 12:38:21 -0300

    Scripts/ICC: Añadida spell Plague Stench a Boss Festergut.

---
+++ sql/Madboxpc_update/Dungeon/11_[Dungeon]Icecrown_Citadel.sql
@@ -0,0 +1,7 @@
+-- ICC
+
+-- BOSS FESTERGUT
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (71160,71161);
+INSERT INTO `spell_script_names` VALUES 
+(71160,'spell_stinky_plague_stench'),
+(71161,'spell_stinky_plague_stench');
