cdbe2f9c8d8c75b5636584a995ae46693d25b072
Retriman <nicolas_katre@hotmail.com>
2011-11-01 13:58:01 -0300

    ICC: Fix de daño de algunos boss.

--- sql/Madboxpc_update/Dungeon/11_[Dungeon]Icecrown_Citadel.sql
+++ sql/Madboxpc_update/Dungeon/11_[Dungeon]Icecrown_Citadel.sql
@@ -19,7 +19,7 @@ UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|1
 -- Lord Marrowgar
 UPDATE `creature_template` SET  `dmg_multiplier` =  '45' WHERE `entry` =36612;-- Valor Anterior 35
 -- Lady Deathwhisper
-UPDATE `creature_template` SET  `dmg_multiplier` =  '10.5' WHERE  `entry` =36855;-- Valor Anterior 7.5
+UPDATE `creature_template` SET  `dmg_multiplier` =  '11.5' WHERE  `entry` =36855;-- Valor Anterior 7.5
 -- Deathbringer
 UPDATE `creature_template` SET  `dmg_multiplier` =  '45' WHERE `entry` =37813;-- Valor Anterior 35
 -- Rotface
@@ -29,4 +29,4 @@ UPDATE `creature_template` SET  `dmg_multiplier` =  '9.5' WHERE  `entry` =36899;
 -- Festergut
 UPDATE `creature_template` SET  `dmg_multiplier` =  '45' WHERE `entry` =36626;-- Valor Anterior 35
 -- Sister Svalna
-UPDATE `creature_template` SET  `dmg_multiplier` =  '10.5' WHERE  `entry` =37126;-- Valor Anterior 7.5
+UPDATE `creature_template` SET  `dmg_multiplier` =  '11.5' WHERE  `entry` =37126;-- Valor Anterior 7.5
