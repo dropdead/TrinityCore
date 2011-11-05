635b2becaecbf12e54ce46eb6aeea505caa7b5f3
Retriman <nicolas_katre@hotmail.com>
2011-11-05 16:09:04 -0300

    Spell: Demonic Circle, Ahora botara la flag en WSGy EOS.

---
+++ sql/Madboxpc_update/Spell/15_[Gral]Demonic_Circle_Bota_Flag.sql
@@ -0,0 +1,5 @@
+-- Demonic Circle: Teleport
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
+( 48020,-23335, 0, 'Bota la flag Demonic Circle: Teleport en WSG'),-- Warsong Gulch
+( 48020,-23333, 0, 'Bota la flag Demonic Circle: Teleport en WSG'),-- Warsong Gulch
+( 48020,-34976, 0, 'Bota la flag Demonic Circle: Teleport en EOS');-- Eye of Storm
