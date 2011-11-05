34642ec2226f873af67584fcdcf8cb83d58eca94
Retriman <nicolas_katre@hotmail.com>
2011-11-05 16:10:54 -0300

    Añadido Clean a anterior sql. sorry.

--- sql/Madboxpc_update/Spell/15_[Gral]Demonic_Circle_Bota_Flag.sql
+++ sql/Madboxpc_update/Spell/15_[Gral]Demonic_Circle_Bota_Flag.sql
@@ -1,4 +1,5 @@
 -- Demonic Circle: Teleport
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=48020;
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
 ( 48020,-23335, 0, 'Bota la flag Demonic Circle: Teleport en WSG'),-- Warsong Gulch
 ( 48020,-23333, 0, 'Bota la flag Demonic Circle: Teleport en WSG'),-- Warsong Gulch
