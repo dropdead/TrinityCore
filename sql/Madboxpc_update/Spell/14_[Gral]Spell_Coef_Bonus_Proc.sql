245017e5f51787b6b6db1774ffc8bb7c1b7e3024
Retriman <nicolas_katre@hotmail.com>
2011-11-06 00:25:15 -0300

    Spell: Fix Coeficiente Chimera Shot -  Rank 1 no se afecta por Sp y toma el coeficiente de Ap de defecto, Gracias @Cepox por ver que faltava el rank 1 en esto. Close #349

--- sql/Madboxpc_update/Spell/14_[Gral]Spell_Coef_Bonus_Proc.sql
+++ sql/Madboxpc_update/Spell/14_[Gral]Spell_Coef_Bonus_Proc.sql
@@ -18,11 +18,12 @@ UPDATE `spell_bonus_data` SET `dot_bonus`=0.0653 WHERE `entry` IN (48450, 48451,
 -- -----------------------
 UPDATE `spell_bonus_data` SET `direct_bonus` = 0, `dot_bonus` = 0 WHERE `entry` IN (3044,3674,13812,13797,1978,42243);
 UPDATE `spell_bonus_data` SET `ap_dot_bonus` = 0.1 WHERE `entry` = 13812;
-DELETE FROM `spell_bonus_data` WHERE `entry` IN (24131,53353,53254,53215,53216,53217,53352);
+DELETE FROM `spell_bonus_data` WHERE `entry` IN (24131,53353,53254,53215,53216,53217,53352,53209);
 INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) 
 VALUES
 (24131,0,0,-1,-1,'Hunter - Wyvern Sting (triggered)'),
 (53353,0,0,-1,-1,'Hunter - Chimera Shot (Serpent)'),
+(53209,0,0,-1,-1,'Hunter - Chimera Shot -  Rank 1'),
 (53254 ,0,0,0,0,'Hunter - Wild Quiver Auto shot (triggered)'),
 (53215,0,0,0,0,'Hunter - Wild Quiver Auto shot Rank1'),
 (53216 ,0,0,0,0,'Hunter - Wild Quiver Auto shot Rank2'),
