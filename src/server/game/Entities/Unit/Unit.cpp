bb14a7a452134318a4e00980838ce341d16d42ea
Retriman <nicolas_katre@hotmail.com>
2011-11-23 18:23:56 -0300

    Core/Mechanic: Stheal  no es removido si el daño es abosorvido.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -14662,6 +14662,10 @@ void Unit::ProcDamageAndSpellFor(bool isVictim, Unit* target, uint32 procFlag, u
                     break;
             }
         }
+        // Stealth no es removido si el daño es abosorvido.
+        if ((procFlag & PROC_FLAG_TAKEN_DAMAGE) && !damage)
+            takeCharges = false;
+
         // Remove charge (aura can be removed by triggers)
         if (useCharges && takeCharges)
             i->aura->DropCharge(AURA_REMOVE_BY_EXPIRE);
