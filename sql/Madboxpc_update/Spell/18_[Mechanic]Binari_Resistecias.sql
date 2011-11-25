2125e144f77e03c99a77af11e2a0cc0a6f997d7d
Retriman <nicolas_katre@hotmail.com>
2011-11-25 19:43:16 -0300

    Core/Mechanic: Añadido Sql a reistencias binarias.

---
+++ sql/Madboxpc_update/Spell/18_[Mechanic]Binari_Resistecias.sql
@@ -0,0 +1,7 @@
+DELETE FROM `playercreateinfo_spell` WHERE `Spell` IN ('199995');
+INSERT INTO `playercreateinfo_spell` (`race`, `class`, `Spell`, `Note`) VALUES
+('5','9','199995','Chaos Bolt Passive (SERVERSIDE)'),
+('2','9','199995','Chaos Bolt Passive (SERVERSIDE)'),
+('1','9','199995','Chaos Bolt Passive (SERVERSIDE)'),
+('7','9','199995','Chaos Bolt Passive (SERVERSIDE)'),
+('10','9','199995','Chaos Bolt Passive (SERVERSIDE)');
