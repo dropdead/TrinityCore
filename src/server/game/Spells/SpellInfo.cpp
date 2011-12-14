8b865ee8e44632cf55aaf6089325e6d8099d6fc3
Cepox <bomkech2@yahoo.com>
2011-12-14 19:45:04 -0300

    [Core/Spells] Druid - Faerie Fire, traspasaba auras de inmunidad

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -1240,8 +1240,8 @@ bool SpellInfo::CanPierceImmuneAura(SpellInfo const* aura) const
     if (Attributes & SPELL_ATTR0_UNAFFECTED_BY_INVULNERABILITY)
         return true;
 
-    // these spells (Cyclone for example) can pierce all...
-    if ((AttributesEx & SPELL_ATTR1_UNAFFECTED_BY_SCHOOL_IMMUNE)
+    // these spells (Cyclone for example) can pierce all... impedir que faerie fire traspase immuneauras
+    if ((AttributesEx & SPELL_ATTR1_UNAFFECTED_BY_SCHOOL_IMMUNE && !(SpellIconID == 109))
         // ...but not these (Divine shield for example)
         && !(aura && (aura->Mechanic == MECHANIC_IMMUNE_SHIELD || aura->Mechanic == MECHANIC_INVULNERABILITY)))
         return true;
