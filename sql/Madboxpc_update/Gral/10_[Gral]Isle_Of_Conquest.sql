bcf793d8234cc1b4def439d040b1708391b8ee33
Retriman <nicolas_katre@hotmail.com>
2011-12-17 22:34:07 -0300

    Core/Bg: Parcial Fix para que no puedan agrear ni pegarle a los boss de isle of conquest sin destruir, las puertas antes.

---
+++ sql/Madboxpc_update/Gral/10_[Gral]Isle_Of_Conquest.sql
@@ -0,0 +1,4 @@
+-- bosses Isle of Conquest
+UPDATE `creature_template` SET `unit_flags` = 0  WHERE `entry` in (34924,35403, 34922,35405);
+UPDATE `creature_template` SET `faction_A` = 35, `faction_H` = 35 WHERE `entry` in (34924,35403);
+UPDATE `creature_template` SET `faction_A` = 35, `faction_H` = 35 WHERE `entry` in (34922,35405);
