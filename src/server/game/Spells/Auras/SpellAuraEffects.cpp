238df6878f02a22ef4a4925002ed2052ef6687a3
Cepox <bomkech2@yahoo.com>
2012-01-01 00:36:46 -0300

    [Core/Spells] Inmunidades de Bladestorm

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -3363,7 +3363,10 @@ void AuraEffect::HandleModStateImmunityMask(AuraApplication const* aurApp, uint8
         immunity_list.pop_back(); // delete Disarm
         immunity_list.push_back(SPELL_AURA_MOD_PACIFY_SILENCE);
         target->ApplySpellImmune(GetId(), IMMUNITY_EFFECT, SPELL_EFFECT_KNOCK_BACK, apply);
-        target->ApplySpellImmune(GetId(), IMMUNITY_MECHANIC, MECHANIC_SNARE, apply); 
+        target->ApplySpellImmune(GetId(), IMMUNITY_MECHANIC, MECHANIC_SNARE, apply);
+        // Inmunidad a mind control y hex
+        target->ApplySpellImmune(GetId(), IMMUNITY_MECHANIC, MECHANIC_CHARM, apply);
+        target->ApplySpellImmune(GetId(), IMMUNITY_MECHANIC, MECHANIC_POLYMORPH, apply);
     }
 
     if (apply && GetSpellInfo()->AttributesEx & SPELL_ATTR1_DISPEL_AURAS_ON_IMMUNITY)
